428e686286a296fc39ab28cafb9fe8ba3b9bb694
nkurniansyah <nuzulul.kurniansyah1992@gmail.com>
2021-09-29 11:41:59 -0400

    AUC

--- Code/Utils.R
+++ Code/Utils.R
@@ -21,8 +21,31 @@ standardize_prs<-function(prs_score,mean_prs, sd_prs){
 
 
 
-generate_auc<-function(pheno,outcome, unrels ){
+generate_auc<-function(pheno,outcome, covars_prs ){
   
+  set.seed(888)
+  
+  sample <- sample(nrow(pheno),floor(nrow(pheno)*0.8))
+  
+  train <- pheno[sample,]
+  test <- pheno[-sample,]
+  
+
+  model_fit<- glm(as.formula(paste(outcome , "~ ", paste(covars_prs, collapse = "+"))), family = "binomial",data = train)
+  
+  pred_mod <- predict(model_fit, test, type="response")
+  
+  pred_mod<-data.frame(pred=pred_mod)
+ 
+  test_dat<- cbind(test,pred_mod)
+  
+
+  auc_prs= ci.auc(roc(test_dat[,outcome] ~ test_dat$pred), conf.level=0.95, method=c("bootstrap") ,boot.n = 2000, boot.stratified = TRUE, reuse.auc=TRUE,
+                 progress = getOption("pROCProgress")$name, parallel=FALSE)
+  auc_prs_df<- c(auc_prs)
+  names(auc_prs_df)<- c("auc_low","auc","auc_hi") 
+  
+  return(auc_prs_df)
   
 }
 
